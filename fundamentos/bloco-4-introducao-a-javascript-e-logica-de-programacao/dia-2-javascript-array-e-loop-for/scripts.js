e0a2a974f31628c4277d34ddd7cc78243ec07276
Lucas Peres <lucasperesss@gmail.com>
2021-07-30 08:05:39 -0300

    Exercícios até o 9

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/scripts.js
@@ -0,0 +1,75 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27]
+
+// Exercício 1
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   console.log(numbers[index])
+// }
+
+// Exercício 2
+
+// let soma = 0
+// for (let index = 0; index < numbers.length; index += 1) {
+//   soma += numbers[index]
+// }
+// console.log(soma)
+
+// Exercícios 3 e 4
+
+// let soma = 0
+// let media = 0
+// for (let index = 0; index < numbers.length; index += 1) {
+//   soma += numbers[index]
+// }
+// media = soma/numbers.length
+// if (media > 20) {
+//   console.log('Valor maior que 20')
+// }
+// else {console.log('Valor menor ou igual a 20')}
+
+// Exercício 5
+
+// let maior = null
+// for (let index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] > maior) {
+//     maior = numbers[index]
+//   }
+// }
+// console.log(maior)
+
+// Exercício 6
+
+// let nImpares = 0
+// for (let index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] % 2 !== 0) {
+//     nImpares += 1
+//   }
+// }
+// if (nImpares > 0) {
+//   console.log(nImpares)
+// }
+// else {console.log('Nenhum valor ímpar encontrado')}
+
+// Exercício 7
+
+// let menor = Number.MAX_VALUE
+// for (let index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] < menor) {
+//     menor = numbers[index]
+//   }
+// }
+// console.log(menor)
+
+// Exercício 8
+
+// let exercicio8 = []
+// for (let index = 1; index <= 25; index += 1) {
+//   exercicio8.push(index)
+// }
+// console.log(exercicio8)
+
+// Exercício 9
+
+// for (let i = 0; i < exercicio8.length; i += 1) {
+//   console.log(exercicio8[i]/2)
+// }
