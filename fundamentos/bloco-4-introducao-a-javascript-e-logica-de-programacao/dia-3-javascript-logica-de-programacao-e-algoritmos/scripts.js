23346b30aa83f3cd86ed12abe960a965bab80fd1
Lucas Peres <lucasperesss@gmail.com>
2021-08-01 20:35:26 -0300

    Até o exercício 3

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -0,0 +1,58 @@
+
+// Exercício 1
+// let n = 5
+
+// if (n <= 1) {
+//   console.log('A variável \'n\' precisa ser maior que 1')
+// }
+// else {
+//   for (let i = 1; i <= n; i += 1) {
+//     let linha = ''
+//     for (let j = 1; j <= n; j += 1) {
+//       linha = linha + '*'
+//     }
+//     console.log(linha)
+//   }
+// }
+
+// Exercício 2
+
+// let n = 5
+
+// if (n <= 1) {
+//   console.log('A variável \'n\' precisa ser maior que 1')
+// }
+// else {
+//   for (let i = 1; i <= n; i += 1) {
+//     let linha = ''
+//     for (let j = 1; j <= i; j += 1) {
+//       linha = linha + '*'
+//     }
+//     console.log(linha)
+//   }
+// }
+
+// Exercício 3
+
+// let n = 15
+// let ne = n - 2
+// let na = 1
+
+// if (n <= 1) {
+//   console.log('A variável \'n\' precisa ser maior que 1')
+// }
+// else {
+
+//   for (let l = 1; l <= n; l += 1) {
+//     let linha = ''
+//     for (let space = ne; space >= 0; space -= 1) {
+//       linha += ' '
+//     }
+//     for (let a = 1; a <= na; a += 1) {
+//       linha += '*'
+//     }
+//     console.log(linha)
+//     ne -= 1
+//     na += 1
+//   }
+// }
