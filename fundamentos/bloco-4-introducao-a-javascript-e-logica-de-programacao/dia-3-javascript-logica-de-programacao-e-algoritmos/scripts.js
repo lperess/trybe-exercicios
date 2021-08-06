13d6c48bde63f04cf8798528480862ac6a3daedd
Lucas Peres <lucasperesss@gmail.com>
2021-08-06 02:42:18 -0300

    Exercícios Bônus

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -59,25 +59,89 @@
 
 // Exercício 4
 
-let n = 5
-let ne = n/2-0.5
-let ee = 0
-let nn = 1
+// let n = 13
+// let ne = n/2-0.5
+// let ee = 0
+// let nn = 1
 
-if (n <= 1 || n % 2 ===0) {
-  console.log('A variável \'n\' precisa ser maior que 1 e ímpar')
-}
-else {
-  for (let l = 1; l < n/2+1; l += 1) {
-    let linha = ''
-    for (let e = ne; e > ee; e -= 1) {
-      linha += ' '
-    }
-    for (let a = 1; a <= nn; a += 1) {
-      linha += '*'
-    }
-    console.log(linha)
-    ee += 1
-    nn += 2
+// if (n <= 1 || n % 2 === 0) {
+//   console.log('A variável \'n\' precisa ser maior que 1 e ímpar')
+// }
+// else {
+//   for (let l = 1; l < n / 2 + 1; l += 1) {
+//     let linha = ''
+//     for (let e = ne; e > ee; e -= 1) {
+//       linha += ' '
+//     }
+//     for (let a = 1; a <= nn; a += 1) {
+//       linha += '*'
+//     }
+//     console.log(linha)
+//     ee += 1
+//     nn += 2
+//   }
+// }
+
+// Exercício 5 (bônus)
+
+// let n = 15
+// let ne = n / 2 - 0.5
+// let ee = 1
+// let ms = 2
+
+// if (n <= 1 || n % 2 === 0) {
+//   console.log('A variável \'n\' precisa ser maior que 1 e ímpar')
+// }
+// else {
+//   for (let l = 1; l < n / 2 + 1; l += 1) {
+//     let linha = ''
+
+//     if (l > 1 && l < n / 2) {
+
+//       for (let e = ne; e > ee; e -= 1) {
+//         linha += ' '
+//       }
+
+//       linha += '*'
+
+//       for (let midSpace = 1; midSpace < ms; midSpace += 1) {
+//         linha += ' '
+//       }
+
+//       linha += '*'
+
+//       ee += 1
+//       ms += 2
+//     }
+
+//     else if (l === 1) {
+//       for (let e = ne; e > 0; e -= 1) {
+//         linha += ' '
+//       }
+//       linha += '*'
+//     }
+//     else if (l > n/2) {
+//       for (let e = 1; e <= n; e += 1) {
+//         linha += '*'
+//       }
+//     }
+//     console.log(linha)
+//   }
+// }
+
+// Exercício 6 (bônus)
+
+let n = 23
+let divisores = 2
+
+for (let i = 2; i < n; i += 1) {
+  if (n % i === 0) {
+    divisores += 1
   }
+}
+if (divisores > 2) {
+  console.log(`${n} não é um número primo`)
+}
+if (divisores === 2) {
+  console.log(`${n} é um número primo!`)
 }
