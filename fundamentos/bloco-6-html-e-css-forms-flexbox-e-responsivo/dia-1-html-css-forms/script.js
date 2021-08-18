af751e8e6c0ecee634a6fc916d8418ed975b37df
Lucas Peres <lucasperesss@gmail.com>
2021-08-18 20:20:24 -0300

    Exercício 1

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,9 @@
+const estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+
+const selectState = document.getElementById('state-select');
+
+for(estado of estados) {
+  const option = document.createElement('option');
+  option.innerHTML = estado;
+  selectState.appendChild(option);
+}
