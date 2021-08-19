d818ba3df2de4f21405eb53046846ee323313c30
Lucas Peres <lucasperesss@gmail.com>
2021-08-19 00:26:36 -0300

    Exercício 2

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,9 +1,24 @@
-const estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
+function fillStatesList() {
+  const estados = ['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Distrito Federal', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins'];
 
-const selectState = document.getElementById('state-select');
+  const stateSelect = document.getElementById('state-select');
 
-for(estado of estados) {
-  const option = document.createElement('option');
-  option.innerHTML = estado;
-  selectState.appendChild(option);
+  for (estado of estados) {
+    const option = document.createElement('option');
+    option.innerText = estado;
+    option.value = estado;
+    stateSelect.appendChild(option);
+  }
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  
+}
+
+window.onload = function initPage() {
+  fillStatesList();
+
+  const submitButton = document.getElementById('button-submit').addEventListener('click', handleSubmit)
 }
