f5c36c09083b50edffa4ebb5300d8fe56e239cdb
Lucas Peres <lucasperesss@gmail.com>
2021-08-30 20:16:14 -0300

    Parte 2 até ex 8

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/parte2.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/parte2.js
@@ -38,3 +38,27 @@ console.log(objLength(lesson3));
 // Exercício 4
 const arrayOfValues = obj => Object.values(obj);
 console.log(arrayOfValues(lesson1))
+
+// Exercício 5
+const allLessons = Object.assign({}, { lesson1, lesson2, lesson3 });
+console.log(allLessons);
+
+// Exercício 6
+const totalStudents = lessons => {
+  let total = 0;
+  const valuesArray = Object.values(lessons);
+  for (let index of valuesArray) {
+    total += index.numeroEstudantes;
+  }
+  console.log(total);
+};
+totalStudents(allLessons);
+
+// Exercício 7
+const valueByIndex = (obj, index) => Object.values(obj)[index];
+console.log(valueByIndex(lesson1, 0));
+
+// Exercício 8
+const verifyKeyValue = (obj, key, value) => obj[key] === value ? true : false;
+console.log(verifyKeyValue(lesson3, 'turno', 'noite'));
+console.log(verifyKeyValue(lesson3, 'materia', 'Maria Clara'));
