d4e894c5cc66cd7fc1cf6049e0b927a89de239bb
Lucas Peres <lucasperesss@gmail.com>
2021-09-01 00:09:53 -0300

    Throw error messages

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/bonus5.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-testes-unitarios-em-javascript/bonus5.js
@@ -1,12 +1,20 @@
 const verify = password => {
-  let isValid = true;
 
-  if (password === null) { isValid = false; }
-  if (password.length <= 8) { isValid = false }
-  if (!/[A-Z]/.test(password)) { isValid = false }
-  if (!/[a-z]/.test(password)) { isValid = false }
-  if (!/[0-9]/.test(password)) { isValid = false }
-
-  return isValid;
+  if (password == null) {
+    throw new Error('password is null');
+  }
+  if (password.length <= 8) {
+    throw new Error('password should be larger than 8 chars');
+  }
+  if (!/[A-Z]/.test(password)) {
+    throw new Error('password should have one uppercase letter at least');
+  }
+  if (!/[a-z]/.test(password)) {
+    throw new Error('password should have one lowercase letter at least');
+  }
+  if (!/[0-9]/.test(password)) {
+    throw new Error('password should have one number at least');
+  }
+  return true;
 }
-console.log(verify('123123123Aa'))
+console.log(verify('1231234Ab'));
